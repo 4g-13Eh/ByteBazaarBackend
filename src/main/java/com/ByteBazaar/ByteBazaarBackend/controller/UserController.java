cfc35d3141ab989852c42af6878b2f9060144b61
4g-13Eh <shahinamon05@gmail.com>
2024-09-04 16:20:06 +0200

    update usercontroller

--- src/main/java/com/ByteBazaar/ByteBazaarBackend/controller/UserController.java
+++ src/main/java/com/ByteBazaar/ByteBazaarBackend/controller/UserController.java
@@ -1,4 +1,17 @@
 package com.ByteBazaar.ByteBazaarBackend.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class UserController {
+
+    @GetMapping("api/users/{id}")
+    String getUserById(@PathVariable("id") String id){
+        return "Hi";
+    }
+
+    @PostMapping("api/users")
+    String createUser(@RequestBody String user){
+        return "Created User with Id: " + user;
+    }
 }
