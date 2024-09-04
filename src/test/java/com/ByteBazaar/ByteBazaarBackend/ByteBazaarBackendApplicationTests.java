33c374e2b05702f5a088591a321f76209527a0ec
4g-13Eh <shahinamon05@gmail.com>
2024-09-04 11:18:31 +0200

    init commit

---
+++ src/test/java/com/ByteBazaar/ByteBazaarBackend/ByteBazaarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ByteBazaar.ByteBazaarBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteBazaarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
