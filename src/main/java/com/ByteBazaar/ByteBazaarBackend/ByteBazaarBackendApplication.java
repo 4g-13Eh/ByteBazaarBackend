7152d8a518e95a29c6a083b138f08c4f5b036160
4g-13Eh <shahinamon05@gmail.com>
2024-09-05 16:32:58 +0200

    update app settings

--- src/main/java/com/ByteBazaar/ByteBazaarBackend/ByteBazaarBackendApplication.java
+++ src/main/java/com/ByteBazaar/ByteBazaarBackend/ByteBazaarBackendApplication.java
@@ -2,8 +2,10 @@ package com.ByteBazaar.ByteBazaarBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.ByteBazaar.ByteBazaarBackend.repository")
 public class ByteBazaarBackendApplication {
 
 	public static void main(String[] args) {
