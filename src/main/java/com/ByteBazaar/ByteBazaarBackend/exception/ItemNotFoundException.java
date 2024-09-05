f2ea9f7d977e2239199a895bc6795b0e6986a013
4g-13Eh <shahinamon05@gmail.com>
2024-09-05 10:02:00 +0200

    add item exceptions

---
+++ src/main/java/com/ByteBazaar/ByteBazaarBackend/exception/ItemNotFoundException.java
@@ -0,0 +1,7 @@
+package com.ByteBazaar.ByteBazaarBackend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Item not found")
+public class ItemNotFoundException extends RuntimeException {}
