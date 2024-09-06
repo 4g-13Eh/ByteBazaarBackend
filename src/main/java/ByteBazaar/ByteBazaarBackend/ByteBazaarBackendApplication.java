fdf17b7dcb3727ccca06e1c71fd55dff3e129936
4g-13Eh <shahinamon05@gmail.com>
2024-09-06 13:12:52 +0200

    update testfile

---
+++ src/main/java/ByteBazaar/ByteBazaarBackend/ByteBazaarBackendApplication.java
@@ -0,0 +1,13 @@
+package ByteBazaar.ByteBazaarBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteBazaarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteBazaarBackendApplication.class, args);
+	}
+
+}
