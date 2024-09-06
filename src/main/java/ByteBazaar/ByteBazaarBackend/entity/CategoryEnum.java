024ecd21342473462367b083c06d1ac9eb71fad4
4g-13Eh <shahinamon05@gmail.com>
2024-09-06 16:51:17 +0200

    removed unneccessary imports

--- src/main/java/ByteBazaar/ByteBazaarBackend/entity/CategoryEnum.java
+++ src/main/java/ByteBazaar/ByteBazaarBackend/entity/CategoryEnum.java
@@ -2,9 +2,6 @@ package ByteBazaar.ByteBazaarBackend.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "categories")
