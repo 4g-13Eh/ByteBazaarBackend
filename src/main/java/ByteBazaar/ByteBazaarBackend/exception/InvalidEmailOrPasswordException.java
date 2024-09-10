c82c0b13fd16442ea16cd4ac369c0bbb79811027
4g-13Eh <shahinamon05@gmail.com>
2024-09-10 10:33:25 +0200

    add validation exception

---
+++ src/main/java/ByteBazaar/ByteBazaarBackend/exception/InvalidEmailOrPasswordException.java
@@ -0,0 +1,7 @@
+package ByteBazaar.ByteBazaarBackend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "No email or password")
+public class InvalidEmailOrPasswordException extends RuntimeException{}
