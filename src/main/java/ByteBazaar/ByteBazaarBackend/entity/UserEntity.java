84d4910200f5f02245d742c40775dce8238d4ea2
4g-13Eh <shahinamon05@gmail.com>
2024-09-12 14:53:13 +0200

    implemented auth controller

--- src/main/java/ByteBazaar/ByteBazaarBackend/entity/UserEntity.java
+++ src/main/java/ByteBazaar/ByteBazaarBackend/entity/UserEntity.java
@@ -5,13 +5,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.UuidGenerator;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Entity
 @Table(name = "users")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserEntity {
+public class UserEntity implements UserDetails {
     @Id
     @Column(name = "Id")
     @UuidGenerator
@@ -22,4 +26,39 @@ public class UserEntity {
     private String passwordHash;
     @Column(name = "cartId")
     private String cartId;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return passwordHash;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
--- src/main/java/ByteBazaar/ByteBazaarBackend/security/controller/AuthenticationController.java
+++ src/main/java/ByteBazaar/ByteBazaarBackend/security/controller/AuthenticationController.java
@@ -1,5 +1,12 @@
 package ByteBazaar.ByteBazaarBackend.security.controller;
 
+import ByteBazaar.ByteBazaarBackend.security.dto.JwtTokenDto;
+import ByteBazaar.ByteBazaarBackend.security.dto.SignInDto;
+import ByteBazaar.ByteBazaarBackend.security.dto.SignUpDto;
+import ByteBazaar.ByteBazaarBackend.security.service.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/auth")
 public class AuthenticationController {
 
+    @Autowired
+    private AuthenticationService authenticationService;
 
+    @PostMapping("/signup")
+    JwtTokenDto signup(@RequestBody SignUpDto data){
+        return authenticationService.signup(data);
+    }
+
+    @PostMapping("/signin")
+    JwtTokenDto signin(@RequestBody SignInDto data){
+        return authenticationService.signin(data);
+    }
 }
